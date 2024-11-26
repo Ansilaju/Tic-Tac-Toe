0dfbc8c402f902ba7f8acddedf3b1a9864fc6e95
Ansilaju <ansilajbin@gmail.com>
2024-11-26 19:23:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Square from "./components/Square";
+import Board from "./components/Board";
+import List from "./components/List";
+
+function App() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xisSet, setxisSet] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  const handlePlay = (nextSquares) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setxisSet(!xisSet);
+  };
+
+  return (
+    <div className="game">
+      <div>
+        <h1 className="heading">TIC TAC TOE</h1>
+      </div>
+      <div className="section">
+        <div className="game-board">
+          <Board
+            squares={currentSquares}
+            onPlay={handlePlay}
+            xisSet={xisSet}
+            setxisSet={setxisSet}
+          />
+        </div>
+        <div className="game-info">
+          <List
+            squares={currentSquares}
+            currentMove={currentMove}
+            setCurrentMove={setCurrentMove}
+            history={history}
+            setHistory={setHistory}
+            setxisSet={setxisSet}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
